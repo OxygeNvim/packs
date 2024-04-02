625aff38ac973afc28f2baa31e650528118d7c53
budchirp <budchirp@gmail.com>
2024-04-02 20:01:54 +0300

    feat: add kotlin

---
+++ lua/oxygen/packs/languages/kotlin/init.lua
@@ -0,0 +1,26 @@
+return {
+  { import = 'oxygen.packs.linters.ktlint' },
+
+  {
+    'nvim-treesitter/nvim-treesitter',
+    opts = function(_, opts)
+      table.insert(opts.ensure_installed, { 'kotlin' })
+    end,
+  },
+
+  {
+    'neovim/nvim-lspconfig',
+    opts = {
+      servers = {
+        kotlin_language_server = {},
+      },
+    },
+  },
+
+  {
+    'williamboman/mason-lspconfig.nvim',
+    opts = function(_, opts)
+      table.insert(opts.ensure_installed, 'kotlin_language_server')
+    end,
+  },
+}
